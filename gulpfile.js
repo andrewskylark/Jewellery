1af59e0d906d7a601204c23ffe4f58b25b9ce930
andrewskylark <andrew.skylark@gmail.com>
2021-04-16 18:54:02 +0400

    добавляет шапку

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task(`server`, function () {
   });
 
   gulp.watch(`source/sass/**/*.{scss,sass}`, gulp.series(`css`));
-  gulp.watch(`source/img/icon-*.svg`, gulp.series(`sprite`, `html`, `refresh`));
+  gulp.watch(`source/img/icon_*.svg`, gulp.series(`sprite`, `html`, `refresh`));
   gulp.watch(`source/*.html`, gulp.series(`html`, `refresh`));
   gulp.watch(`source/js/*.js`, gulp.series(`js`, `refresh`));
 });
@@ -68,7 +68,7 @@ gulp.task(`webp`, function () {
 });
 
 gulp.task(`sprite`, function () {
-  return gulp.src(`source/img/{icon-*,htmlacademy*}.svg`)
+  return gulp.src(`source/img/icon_*.svg`)
       .pipe(svgstore({inlineSvg: true}))
       .pipe(rename(`sprite_auto.svg`))
       .pipe(gulp.dest(`build/img`));
